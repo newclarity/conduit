142a245d96af0fd057ffe5f6e9a0e61d8b888f0c
Mike Schinkel <mike@newclarity.net>
2012-03-23 17:49:31 -0400

    First commit.

---
+++ conduit-0.1.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Conduit Integrations Library for WordPress
+ *
+ * @package Conduit
+ * @version 0.1
+ * @author Micah Wood <micah@newclarity.net>
+ * @author Mike Schinkel <mike@newclarity.net>
+ * @license http://opensource.org/licenses/gpl-2.0.php
+ * @copyright Copyright (c) 2012, NewClarity LLC
+ *
+ */
+define( 'CONDUIT', true );
